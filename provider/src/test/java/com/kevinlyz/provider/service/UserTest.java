88eb81f478b6a9d0242bcdadee9ba1d98061152e
kevin <hbukevin@gmail.com>
2020-02-26 20:04:41 +0800

    init(log):引入日志

---
+++ provider/src/test/java/com/kevinlyz/provider/service/UserTest.java
@@ -0,0 +1,22 @@
+package com.kevinlyz.provider.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ * 
+ * @author kevinlyz
+ * @version 1.0
+ * @date 2020-02-26 20:00
+ */
+@SpringBootTest
+@Slf4j
+public class UserTest {
+
+    @Test
+    public void logTest(){
+        log.info("log test");
+    }
+}
